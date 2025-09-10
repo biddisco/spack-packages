7ecd239192166a83667f08493b86435360e1e153
John Biddiscombe <biddisco@cscs.ch>
2025-09-10 10:30:06 +0200

    magic-enum: add master branch version

--- repos/spack_repo/builtin/packages/magic_enum/package.py
+++ repos/spack_repo/builtin/packages/magic_enum/package.py
@@ -14,6 +14,7 @@ class MagicEnum(CMakePackage):
 
     homepage = "https://github.com/Neargye/magic_enum"
     url = "https://github.com/Neargye/magic_enum/archive/refs/tags/v0.9.6.tar.gz"
+    git = "https://github.com/Neargye/magic_enum.git"
 
     maintainers("pranav-sivaraman")
 
@@ -21,6 +22,7 @@ class MagicEnum(CMakePackage):
 
     version("0.9.7", sha256="b403d3dad4ef542fdc3024fa37d3a6cedb4ad33c72e31b6d9bab89dcaf69edf7")
     version("0.9.6", sha256="814791ff32218dc869845af7eb89f898ebbcfa18e8d81aa4d682d18961e13731")
+    version('master', branch="master")
 
     variant("examples", default=False, description="Enable examples")
 
